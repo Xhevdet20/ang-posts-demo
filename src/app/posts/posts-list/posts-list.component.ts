b453b40b2c3c2df7ed1d0d577c0ecc241ea6dc94
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-02-01 16:06:15 +0100

    first commit

---
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css']
+})
+export class PostsListComponent {
+  posts = [
+    // {title: 'First Post', content: 'This is the first post\'s content ' },
+    // {title: 'Second Post', content: 'This is the Second post\'s content ' },
+    // {title: 'Third Post', content: 'This is the Third post\'s content ' }
+  ]
+}
