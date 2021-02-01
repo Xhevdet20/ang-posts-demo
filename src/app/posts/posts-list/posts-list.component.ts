93c9e74605c83cabb57595a0ca56fe06414d2d8f
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-02-01 16:51:54 +0100

    commit 1.14

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
+import { Post } from '../post.model';
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
 export class PostsListComponent {
-  posts = [
+  @Input() posts: Post[] = [
     // {title: 'First Post', content: 'This is the first post\'s content ' },
     // {title: 'Second Post', content: 'This is the Second post\'s content ' },
     // {title: 'Third Post', content: 'This is the Third post\'s content ' }
