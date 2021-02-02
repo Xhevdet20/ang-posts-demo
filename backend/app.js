3da9e9413c4dd393caed1156d2342a94655f2d59
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-02-02 16:31:33 +0100

    3.7 fetching posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res, next) => {
-  console.log('first middleware');
-  next();
-});
 
-app.use((req,res, next) => {
-
-  res.send('Hello from express!');
+app.use('/api/posts', (req,res, next) => {
+  const posts = [
+    {
+      id: 'daff123123',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: 'daff123123',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!'
+    }
+  ];
+  res.status(200).json({
+    message : 'Posts fetched suffesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
